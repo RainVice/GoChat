2bd99998b61bf688b097b2174c5fe0f7aea6ebdb
RainVice <3218229723@qq.com>
2022-05-30 09:00:17 +0800

    添加发送接收图片，聊天界面显示图片

--- app/src/main/java/com/rainvice/sockettest_1/activity/ChatActivity.java
+++ app/src/main/java/com/rainvice/sockettest_1/activity/ChatActivity.java
@@ -1,21 +1,41 @@
 package com.rainvice.sockettest_1.activity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.NotificationManager;
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.rainvice.sockettest_1.Adapter.RvAdapter;
 import com.rainvice.sockettest_1.R;
 import com.rainvice.sockettest_1.constant.Status;
@@ -30,12 +50,13 @@ import com.rainvice.sockettest_1.protocol.RvRequestProtocol;
 import com.rainvice.sockettest_1.protocol.RvResponseProtocol;
 import com.rainvice.sockettest_1.server.SendMessageServer;
 import com.rainvice.sockettest_1.utils.DataUtil;
-import com.rainvice.sockettest_1.utils.IpUtil;
 import com.rainvice.sockettest_1.utils.LogUtil;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -62,11 +83,15 @@ public class ChatActivity extends AppCompatActivity {
     @BindView(R.id.send)
     Button mButton;
 
+    @BindView(R.id.image)
+    ImageView mImageView;
+
     private String mIp = "127.0.0.1";
     private String mUsername = "用户名";
     private DialogueRecordBean mDialogueRecordBean;
     private Map<String, DialogueRecordBean> mMessageMap;
     private RvAdapter<DialogBean> mRvAdapter;
+    private ActivityResultLauncher<Intent> mLauncher;
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -76,6 +101,35 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
         ButterKnife.bind(this);
         EventBus.getDefault().register(this);
+
+        mLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                assert data != null;
+                Uri uri = data.getData();
+                ContentResolver resolver = getContentResolver();
+                Bitmap mBitmap = null;
+                try {
+                    //1.将相册返回的uri转为Bitmap
+                    mBitmap = MediaStore.Images.Media.getBitmap(resolver, uri);
+
+                    //2.压缩图片,第二个入参表示图片压缩率，如果是100就表示不压缩
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    mBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bos);
+
+                    //3.将压缩后的图片进行base64编码
+                    byte[] bytes = bos.toByteArray();
+                    String s = Base64.encodeToString(bytes, Base64.DEFAULT);
+
+                    //4.将base64解码，转为bitmap并显示在imageview中看效果
+//                    byte[] decodedString = Base64.decode(s, Base64.DEFAULT);
+//                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    sendMessage(DataType.IMAGE,s,mBitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         //初始化数据
         initData();
@@ -91,13 +145,91 @@ public class ChatActivity extends AppCompatActivity {
         mButton.setOnClickListener(view -> {
             //发送消息
             String msg = mEditText.getText().toString();
-            sendMessage(msg);
+            sendMessage(DataType.WORD, msg,null);
         });
+
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                int length = editable.length();
+                if (length == 0) {
+                    mImageView.setVisibility(View.VISIBLE);
+                    mButton.setVisibility(View.GONE);
+                } else {
+                    mImageView.setVisibility(View.GONE);
+                    mButton.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        mImageView.setOnClickListener(view -> checkPermission());
+
     }
 
-    private void sendMessage(String msg) {
-        if (Objects.nonNull(msg) && !"".equals(msg)){
-            RvRequestProtocol<String> requestProtocol = new RvRequestProtocol<>(MsgType.MESSAGE, msg);
+    public void getImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        mLauncher.launch(intent);
+    }
+
+
+    private static final int READ_EXTERNAL_STORAGE_CODE = 0;
+
+    /**
+     * 检查权限
+     */
+    private void checkPermission() {
+        //1 先判断有没有权限
+        // context 检查权限的字符串
+        int isPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        //返回值只有两个 授权PERMISSION_GRANTED 拒绝PERMISSION_DENIED
+        if (isPermission == PackageManager.PERMISSION_GRANTED) {
+            //2 如果有这个直接拨打
+            getImage();
+        } else {
+            //3 如果每一那么我们就需要去申请
+            //申请的字符串数组 请求码
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_EXTERNAL_STORAGE_CODE) {
+            //不为空 长度大于0 也就是存在这个
+            if (grantResults.length > 0) {
+                //PackageManager.PERMISSION_GRANTED 允许
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //授予了
+                    //执行代码
+                    getImage();
+                } else {
+                    //如果没有授权的话，可以给用户一个友好提示
+                    Toast.makeText(this, "权限被拒绝，无法获取图片", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    private void sendMessage(DataType dataType, String msg,Bitmap bitmap) {
+        RvRequestProtocol<String> requestProtocol = null;
+        if (Objects.nonNull(msg) && !"".equals(msg)) {
+            if (dataType == DataType.WORD) {
+                requestProtocol = new RvRequestProtocol<>(MsgType.MESSAGE, msg);
+            } else {
+                requestProtocol = new RvRequestProtocol<>(MsgType.IMAGE, msg);
+            }
             SendMessageServer sendMessageServer = new SendMessageServer(mIp, requestProtocol);
             sendMessageServer.sendMsg(new SendMessageServer.Callback() {
                 /**
@@ -107,27 +239,34 @@ public class ChatActivity extends AppCompatActivity {
                 @RequiresApi(api = Build.VERSION_CODES.N)
                 @Override
                 public void success(RvResponseProtocol<String> result) {
+                    mEditText.setText("");
                     mTextView.setText(mUsername);
                     //Toast.makeText(ChatActivity.this, "回复状态" + result.getStatus(), Toast.LENGTH_SHORT).show();
                     //获取消息记录
                     String format = new SimpleDateFormat("HH:mm:ss").format(new Date());
-                    DialogBean dialogBean = new DialogBean(true, format, DataType.WORD, msg,true);
+                    DialogBean dialogBean = null;
+                    if (dataType == DataType.WORD){
+                        dialogBean = new DialogBean(true, format, dataType, msg, true);
+                    }else {
+                        dialogBean = new DialogBean(true,format,dataType,bitmap,true);
+                    }
                     //判断是否有消息记录
-                    if (mDialogueRecordBean == null){
+                    if (mDialogueRecordBean == null) {
                         //添加相关消息记录
-                        mDialogueRecordBean = new DialogueRecordBean(mUsername,mIp);
+                        mDialogueRecordBean = new DialogueRecordBean(mUsername, mIp);
                         mDialogueRecordBean.getDialogs().add(dialogBean);
-                        mMessageMap.put(mIp,mDialogueRecordBean);
+                        mMessageMap.put(mIp, mDialogueRecordBean);
                         initRecyclerView();
-                    }else{
+                    } else {
                         List<DialogBean> dialogs = mDialogueRecordBean.getDialogs();
                         int position = dialogs.size();
                         dialogs.add(dialogBean);
-                        mRvAdapter.notifyItemRangeInserted(position,1);
+                        mRvAdapter.notifyItemRangeInserted(position, 1);
                         mRecyclerView.scrollToPosition(position);
                     }
                     mDialogueRecordBean.setTimes(System.currentTimeMillis());
                 }
+
                 /**
                  * 发送失败或者对方掉线
                  * @param result
@@ -146,9 +285,9 @@ public class ChatActivity extends AppCompatActivity {
     public void onEvent(BusToNearbyEvent event) {
         if (event.getStatus() == Status.SUCCESS) {
             String s = DataUtil.getNameMap().get(mIp);
-            if (s == null){
+            if (s == null) {
                 mTextView.setText(mUsername + "(对方可能已掉线)");
-            }else {
+            } else {
                 mUsername = s;
                 mTextView.setText(mUsername);
             }
@@ -160,21 +299,23 @@ public class ChatActivity extends AppCompatActivity {
     public void onEvent(BusToChatEvent event) {
         int status = event.getStatus();
         LogUtil.d("接收到刷新指令", String.valueOf(status));
-        switch (status){
+        switch (status) {
             case Status.SUCCESS:
-                if (mDialogueRecordBean == null){
+                if (mDialogueRecordBean == null) {
                     mDialogueRecordBean = mMessageMap.get(mIp);
                     initRecyclerView();
-                }else {
+                } else {
                     List<DialogBean> dialogs = mDialogueRecordBean.getDialogs();
                     int position = dialogs.size() - 1;
-                    mRvAdapter.notifyItemRangeInserted(position,1);
+                    mRvAdapter.notifyItemRangeInserted(position, 1);
                     mRecyclerView.scrollToPosition(position);
                 }
                 break;
 
         }
-    };
+    }
+
+    ;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void initView() {
@@ -187,37 +328,56 @@ public class ChatActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void initRecyclerView() {
 
-        if (mDialogueRecordBean != null){
+        if (mDialogueRecordBean != null) {
 
             List<DialogBean> dialogs = mDialogueRecordBean.getDialogs();
             dialogs.forEach(dialog -> dialog.setRead(true));
             mRvAdapter = new RvAdapter<>(dialogs, R.layout.item_chat_list, (itemView, position, dialogBean) -> {
-                LinearLayout left = itemView.findViewById(R.id.left_message);
-                LinearLayout right = itemView.findViewById(R.id.right_message);
-                dialogBean.setRead(true);
+                    LinearLayout left = itemView.findViewById(R.id.left_message);
+                    LinearLayout right = itemView.findViewById(R.id.right_message);
+                    dialogBean.setRead(true);
 
-                if(dialogBean.isMine()){
-                    left.setVisibility(View.GONE);
+                    if (dialogBean.isMine()) {
+                        left.setVisibility(View.GONE);
 
-                    TextView time = itemView.findViewById(R.id.right_time);
-                    TextView content = itemView.findViewById(R.id.right_content);
+                        TextView time = itemView.findViewById(R.id.right_time);
+                        TextView content = itemView.findViewById(R.id.right_content);
+                        ImageView imageView = itemView.findViewById(R.id.right_image);
 
-                    time.setText(dialogBean.getTime());
-                    content.setText((String)dialogBean.getContent());
-                }else{
-                    right.setVisibility(View.GONE);
+                        if (dialogBean.getDataType() == DataType.WORD){
+                            imageView.setVisibility(View.GONE);
+                            time.setText(dialogBean.getTime());
+                            content.setText((String) dialogBean.getContent());
+                        }else {
+                            content.setVisibility(View.GONE);
+                            time.setText(dialogBean.getTime());
+                            imageView.setImageBitmap((Bitmap) dialogBean.getContent());
+                        }
 
-                    TextView time = itemView.findViewById(R.id.left_time);
-                    TextView content = itemView.findViewById(R.id.left_content);
 
-                    time.setText(dialogBean.getTime());
-                    content.setText((String)dialogBean.getContent());
-                }
+                    } else {
+                        right.setVisibility(View.GONE);
 
+
+                        TextView time = itemView.findViewById(R.id.left_time);
+                        TextView content = itemView.findViewById(R.id.left_content);
+                        ImageView imageView = itemView.findViewById(R.id.left_image);
+
+                        if (dialogBean.getDataType() == DataType.WORD){
+                            imageView.setVisibility(View.GONE);
+                            time.setText(dialogBean.getTime());
+                            content.setText((String) dialogBean.getContent());
+                        }else {
+                            content.setVisibility(View.GONE);
+                            time.setText(dialogBean.getTime());
+                            Bitmap bit = (Bitmap) dialogBean.getContent();
+                            imageView.setImageBitmap(bit);
+                        }
+                    }
             });
             mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
             mRecyclerView.setAdapter(mRvAdapter);
-            mRecyclerView.scrollToPosition(mRvAdapter.getItemCount()-1);
+            mRecyclerView.scrollToPosition(mRvAdapter.getItemCount() - 1);
 
         }
 
@@ -237,4 +397,6 @@ public class ChatActivity extends AppCompatActivity {
         super.onDestroy();
         EventBus.getDefault().unregister(this);
     }
+
+
 }
--- app/src/main/java/com/rainvice/sockettest_1/service/SocketServerService.java
+++ app/src/main/java/com/rainvice/sockettest_1/service/SocketServerService.java
@@ -7,13 +7,16 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Base64;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import com.rainvice.sockettest_1.R;
@@ -41,7 +44,7 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Random;
 
-@RequiresApi(api = Build.VERSION_CODES.N)
+@RequiresApi(api = Build.VERSION_CODES.S)
 public class SocketServerService extends Service {
     String TAG = this.getClass().getSimpleName();
 
@@ -58,14 +61,13 @@ public class SocketServerService extends Service {
                 //获取消息类型
                 String type = protocol.getType();
                 //获取当前时间
-                String format = new SimpleDateFormat("HH:mm:ss").format(new Date());
+                String time = new SimpleDateFormat("HH:mm:ss").format(new Date());
                 //判断消息类型
                 if (type.equals(MsgType.MESSAGE)) {
                     //处理文字消息
-                    manageWord(ip, protocol, format);
+                    manageWord(ip, protocol, time);
                 } else if (type.equals(MsgType.IMAGE)) {
-
-
+                    manageImage(ip,protocol,time);
                 }
                 EventBus.getDefault().post(new BusToChatEvent(Status.SUCCESS));
                 break;
@@ -80,53 +82,60 @@ public class SocketServerService extends Service {
     });
 
     /**
+     * 处理图片消息
+     *
+     * @param ip       发送者的 ip
+     * @param protocol 消息内容
+     * @param time     时间
+     */
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void manageImage(String ip, RvRequestProtocol<String> protocol, String time) {
+        //4.将base64解码，转为bitmap并显示在imageview中看效果
+        byte[] decodedString = Base64.decode(protocol.getData(), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        //创建消息
+        DialogBean dialogBean = new DialogBean(false, time, DataType.IMAGE,bitmap);
+        DialogueRecordBean dialogueRecordBean = saveDialog(ip, dialogBean);
+        long count = dialogueRecordBean.getDialogs().stream().filter(dialog -> !dialog.isRead()).count();
+        sendNotice(ip, "收到图片消息" + (count == 1 ? "" : "\n" + count + "条未读"), dialogueRecordBean);
+    }
+
+    /**
      * 处理文字消息
      *
      * @param ip       发送者的 ip
      * @param protocol 消息内容
-     * @param format   时间
+     * @param time     时间
      */
-    private void manageWord(String ip, RvRequestProtocol<String> protocol, String format) {
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void manageWord(String ip, RvRequestProtocol<String> protocol, String time) {
         //消息内容
         String data = protocol.getData();
+        //保存消息
+        DialogBean dialogBean = new DialogBean(false, time, DataType.WORD, data);
+        DialogueRecordBean dialogueRecordBean = saveDialog(ip, dialogBean);
+
+        long count = dialogueRecordBean.getDialogs().stream().filter(dialog -> !dialog.isRead()).count();
+        sendNotice(ip, data + (count == 1 ? "" : "\n" + count + "条未读"), dialogueRecordBean);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void sendNotice(String ip, String text, DialogueRecordBean dialogueRecordBean) {
         //获取发送者的昵称
         String username = DataUtil.getNameMap().get(ip);
-        //保存消息
-        Map<String, DialogueRecordBean> messageMap = DataUtil.getMessageMap();
-        DialogBean dialogBean = new DialogBean(false, format, DataType.WORD, data);
-        DialogueRecordBean dialogueRecordBean = messageMap.get(ip);
-        //判断是否有消息记录
-        if (dialogueRecordBean != null) {
-            //添加消息
-            dialogueRecordBean.setUsername(username);
-            dialogueRecordBean.getDialogs().add(dialogBean);
-        } else {
-            //添加消息记录
-            dialogueRecordBean = new DialogueRecordBean(username, ip);
-            dialogueRecordBean.setUsername(username);
-            //添加消息
-            dialogueRecordBean.getDialogs().add(dialogBean);
-            messageMap.put(ip, dialogueRecordBean);
-        }
-        dialogueRecordBean.setTimes(System.currentTimeMillis());
-        EventBus.getDefault().post(new BusToMessageEvent(Status.SUCCESS));
-        //Toast.makeText(this, "接收消息成功，内容是：" + data, Toast.LENGTH_SHORT).show();
-
-
         Intent intent = new Intent(this, ChatActivity.class);
         intent.putExtra(IntentConstant.IP, ip);
         intent.putExtra(IntentConstant.USERNAME, username);
         int noticeId = IpUtil.getIp(ip);
-        PendingIntent pendingIntent =  PendingIntent.getActivity(this , noticeId , intent, PendingIntent.FLAG_MUTABLE);
-
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, noticeId, intent, PendingIntent.FLAG_MUTABLE);
 
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String channelId = "message";
-            long count = dialogueRecordBean.getDialogs().stream().filter(dialog -> !dialog.isRead()).count();
             Notification notification = new Notification.Builder(this, channelId)
                     .setContentTitle(username + "发来一条消息")
-                    .setContentText(data + (count == 1 ? "" : "\n" + count + "条未读"))
+                    .setContentText(text)
                     .setWhen(System.currentTimeMillis())
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setContentIntent(pendingIntent)
@@ -145,7 +154,7 @@ public class SocketServerService extends Service {
             long count = dialogueRecordBean.getDialogs().stream().filter(dialog -> !dialog.isRead()).count();
             Notification notification = new Notification.Builder(this)
                     .setContentTitle(username + "发来一条消息")
-                    .setContentText(data + (count == 1 ? "" : "\n" + count + "条未读"))
+                    .setContentText(text)
                     .setContentIntent(pendingIntent)
                     .setAutoCancel(true)
                     .setSmallIcon(R.mipmap.ic_launcher)
@@ -155,7 +164,39 @@ public class SocketServerService extends Service {
             // 发送通知
             manager.notify(noticeId, notification);
         }
+    }
 
+    /**
+     * 保存消息
+     * @param ip 发送者的 ip 地址
+     * @param dialogBean 消息内容
+     * @return
+     */
+    @NonNull
+    private DialogueRecordBean saveDialog(String ip, DialogBean dialogBean) {
+        //消息列表
+        Map<String, DialogueRecordBean> messageMap = DataUtil.getMessageMap();
+        //获取发送者的昵称
+        String username = DataUtil.getNameMap().get(ip);
+
+        DialogueRecordBean dialogueRecordBean = messageMap.get(ip);
+        //判断是否有消息记录
+        if (dialogueRecordBean != null) {
+            //添加消息
+            dialogueRecordBean.setUsername(username);
+            dialogueRecordBean.getDialogs().add(dialogBean);
+        } else {
+            //添加消息记录
+            dialogueRecordBean = new DialogueRecordBean(username, ip);
+            dialogueRecordBean.setUsername(username);
+            //添加消息
+            dialogueRecordBean.getDialogs().add(dialogBean);
+            messageMap.put(ip, dialogueRecordBean);
+        }
+        dialogueRecordBean.setTimes(System.currentTimeMillis());
+        EventBus.getDefault().post(new BusToMessageEvent(Status.SUCCESS));
+        //Toast.makeText(this, "接收消息成功，内容是：" + data, Toast.LENGTH_SHORT).show();
+        return dialogueRecordBean;
     }
 
 
